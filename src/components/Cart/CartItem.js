d406edc879430a008808572b74090bc467ae1b50
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-15 00:32:22 +0100

    add dispatch to cartitem

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,6 +1,9 @@
+import { useDispatch } from "react-redux";
 import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
+  const dispatch = useDispatch();
+
   const { title, quantity, total, price } = props.item;
 
   return (
