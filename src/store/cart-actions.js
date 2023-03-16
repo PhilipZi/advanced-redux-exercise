c5a9d5632e92222bd1224bb461c6a2cb934ec014
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-16 16:29:08 +0100

    add new state to cart slice change useffect in app js with ternary

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -47,7 +52,13 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         "https://advanced-redux-exercise-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        { method: "PUT", body: JSON.stringify(cart) }
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
       if (!response.ok) {
         throw new Error("Sending cart data failed.");
